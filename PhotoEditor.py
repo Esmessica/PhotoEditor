d8ce28e87fc4c64d5fa07f181717eae4b9d22c6e
Anna Lorencová <a.lorencov@seznam.cz>
2023-04-14 09:11:20 +0200

    First Commit

---
+++ PhotoEditor.py
@@ -0,0 +1,16 @@
+from PIL import Image, ImageEnhance, ImageFilter
+import os
+
+path = "./imgs"
+pathOut = "/editedImgs"
+"""Paths for img editor and path to folder for output imgs after edit"""
+
+"""For loop for editting each img and putting it to output folder"""
+for filename in os.listdir(path):
+    img = Image.open(f"{path}/{filename}")
+    edit = img.filter(ImageFilter.SHARPEN).convert("L")         # Apply filter SHARPEN, convert for color mode etc.
+    factor = 1.5                                                # Intensity of enhance
+    enhancer = ImageEnhance.Contrast(edit)                      # Define Contrast in file from variable edit
+    edit = enhancer.enhance(factor)                             # Apply Contrast in file from variable edit
+    clean_name = os.path.splitext(filename)[0]                  # Cleaning name of the file to match starting name
+    edit.save(f".{pathOut}/{clean_name}_edited.jpg")            # Saving the img to the defined folder
